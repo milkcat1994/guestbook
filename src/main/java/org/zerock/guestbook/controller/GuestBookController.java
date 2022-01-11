bfa9524b5f87d6953b3aa1b7c58c0e12d12588ce
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-11 16:05:09 +0900

    Guestbook 등록 페이지 기능 구현

--- src/main/java/org/zerock/guestbook/controller/GuestBookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestBookController.java
@@ -5,7 +5,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -26,5 +29,23 @@ public class GuestBookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list.........."+ pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    // 화면 도식
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    // 등록 처리
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..." +dto);
+
+        // 새로 추가된 Entity 번호
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
