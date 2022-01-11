77bed66e6f0fb14c47f7249c385669fd0ea27b7c
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-11 13:50:34 +0900

    Guestbook 등록 서비스 구현 및 DTO를 엔티티로 변환

--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,15 +1,31 @@
 package org.zerock.guestbook.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor // 의존성 자동 주입
 public class GuestbookServiceImpl implements GuestbookService{
+
+    // 반드시 final로 선언
+    // @RequiredArgsConstructor로 인해 초기화 되지않은 final 필드, @NonNull이 붙은 필드를 매개변수로 갖는 생성자를 자동으로 생성한다.
+    private final GuestbookRepository repository;
 
     @Override
     public Long register(GuestbookDTO dto) {
-        return null;
+        log.info("DTO-----------------------------");
+        log.info(dto);
+
+        // Guestbook의 DTO를 JPA에서 처리하기 위해 Entity로 변경
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
+
+        repository.save(entity);
+        return entity.getGno();
     }
 }
