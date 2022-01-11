afb20b89aa0e8b3bb6e5e2721adfd182354fc95b
Yang Ji Yong <milkcat1994@gmail.com>
2022-01-11 13:23:25 +0900

    Querydsl 테스트 - 단일 항목 검색 테스트

--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -1,9 +1,16 @@
 package org.zerock.guestbook.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -39,5 +46,32 @@ public class GuestbookRepositoryTests {
 
             guestbookRepository.save(guestbook);
         }
+    }
+
+    // 제목에 1 이라는 글자가 있는 엔티티 검색
+    @Test
+    public void testQuery1(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+        // 동적으로 처리하기 위한 Q도메인 클래스 가져오기
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+        String keyword = "1";
+
+        // where문에 들어가는 조건들을 넣어주는 컨테이너
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 원하는 조건은 필드 값과 같이 결합해서 생성
+        // java의 Predicate 타입이 아님
+        BooleanExpression expression = qGuestbook.title.contains(keyword);
+
+        // 만들어진 조건은 where문에 and나 or같은 키워드와 결합
+        builder.and(expression);
+
+        // BooleanBuilder는 GuestbookRepository에 추가된 QuerydslPredicateExcutor 인터페이스의 findAll() 사용가능
+        Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+        result.stream().forEach(guestbook -> {
+            System.out.println(guestbook);
+        });
     }
 }
